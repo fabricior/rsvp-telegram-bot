edb06f2568098f0125b04a858dc1fbbebb16dbcc
Fabricio Rateni <fabricio.rateni@gmail.com>
2022-03-25 16:30:23 -0300

    adding telegram Id and minor schema adjustments

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -2,7 +2,9 @@ import { PrismaClient } from "@prisma/client";
 const prismaClient = new PrismaClient();
 
 async function seed() {
-  await prismaClient.user.create({ data: { firstName: "Fabricio" } });
+  await prismaClient.user.create({
+    data: { firstName: "Fabricio", telegramId: 123123 },
+  });
 }
 
 seed();
