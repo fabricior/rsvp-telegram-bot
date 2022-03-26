279dcd1b5670fe35a411aedcf9ff665ceb77e2f4
Fabricio Rateni <fabricio.rateni@gmail.com>
2022-03-26 12:12:48 -0300

    implementation for computeRSVPStatus

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,4 @@
-import { Game } from "@prisma/client";
+import { Game, Rsvp } from "@prisma/client";
 import { db } from "./db";
 
 export async function insertGame(): Promise<Game> {
@@ -20,4 +20,55 @@ export async function getUpcoming(): Promise<Game | null> {
     },
     orderBy: { dateTime: "asc" },
   });
+}
+
+type GameRsvpStatus = {
+  goingCount: number;
+  notGoingCount: number;
+  maybeCount: number;
+};
+
+export function computeRSVPStatus(game: Game) {
+  const userRsvps = getlatestRsvpPerUser(game);
+
+  const initialState: GameRsvpStatus = {
+    goingCount: 0,
+    notGoingCount: 0,
+    maybeCount: 0,
+  };
+
+  return userRsvps.reduce(
+    (previous, current) => ({
+      ...previous,
+      goingCount:
+        current.option === "YES"
+          ? previous.goingCount + 1
+          : previous.goingCount,
+      notGoingCount:
+        current.option === "NO"
+          ? previous.notGoingCount + 1
+          : previous.notGoingCount,
+      maybeCount:
+        current.option === "MAYBE"
+          ? previous.maybeCount + 1
+          : previous.maybeCount,
+    }),
+    initialState
+  );
+}
+
+function getlatestRsvpPerUser(game: Game): Array<Rsvp> {
+  const buildRecord = () =>
+    game.rsvps.reduce((previous, currentItem) => {
+      const groupKey = currentItem.userId;
+      if (
+        !previous[groupKey] ||
+        previous[groupKey].createdAt < currentItem.createdAt
+      ) {
+        previous[groupKey] = currentItem;
+      }
+      return previous;
+    }, {} as Record<string, Rsvp>);
+
+  return Object.values(buildRecord());
 }
