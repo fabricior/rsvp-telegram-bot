8fe62f40d8bc7d373c78b9b0c1764584e94cb30e
Fabricio Rateni <fabricio.rateni@gmail.com>
2022-04-01 11:39:10 -0300

    removing commented out code

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,3 @@
 import setup from "./bot";
 
 setup()
-
-// // import { getUpcoming, insertGame } from "./game";
-// // import { rsvpViaTelegram } from "./rsvp";
-
-// // async function test_with_seed() {
-// //   console.log("simple test_with_seed");
-
-// //   await insertGame({
-// //     dateTime: new Date(Date.UTC(2024, 4, 4, 23, 0, 0)),
-// //     requiredPlayers: 10,
-// //   });
-
-// //   const game = await getUpcoming();
-// //   if (!game) {
-// //     throw Error("No upcoming games");
-// //   }
-
-// //   return rsvpViaTelegram({
-// //     gameId: game.id,
-// //     rsvpOption: "MAYBE",
-// //     telegramUserId: 123456,
-// //   });
-// // }
-
-// // const promise = test_with_seed();
-
-// // promise.then((result) => console.log(result));
