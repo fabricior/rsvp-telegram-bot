74f5159b9594af5a3683ed28925aad9f354340f7
Fabricio Rateni <fabricio.rateni@gmail.com>
2022-05-12 12:12:27 -0300

    adding healthcheck endpoint

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import dotenv from "dotenv";
 import setupBot from "./bot";
+import startHealthCheckServer from "./healthCheck";
 
 dotenv.config();
-setupBot()
-
+setupBot();
+startHealthCheckServer();
